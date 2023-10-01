23033f4ab6218f1d74844062db324c8c3bf32092
Ангальдт Михаил <beat1097@mail.ru>
2023-10-01 17:55:35 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows.Forms;
+
+namespace lab2SH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.RadioButton3 = new System.Windows.Forms.RadioButton();
+            this.textField1 = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(186, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите аргумент х";
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.AutoSize = true;
+            this.CheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.CheckBox.Location = new System.Drawing.Point(360, 304);
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Size = new System.Drawing.Size(119, 28);
+            this.CheckBox.TabIndex = 1;
+            this.CheckBox.Text = "Удвоение";
+            this.CheckBox.UseVisualStyleBackColor = true;
+            this.CheckBox.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(168, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(481, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RadioButton1
+            // 
+            this.RadioButton1.AutoSize = true;
+            this.RadioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.RadioButton1.Location = new System.Drawing.Point(168, 252);
+            this.RadioButton1.Name = "RadioButton1";
+            this.RadioButton1.Size = new System.Drawing.Size(95, 28);
+            this.RadioButton1.TabIndex = 3;
+            this.RadioButton1.TabStop = true;
+            this.RadioButton1.Text = "x*sin(X)";
+            this.RadioButton1.UseVisualStyleBackColor = true;
+            this.RadioButton1.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // RadioButton2
+            // 
+            this.RadioButton2.AutoSize = true;
+            this.RadioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.RadioButton2.Location = new System.Drawing.Point(369, 252);
+            this.RadioButton2.Name = "RadioButton2";
+            this.RadioButton2.Size = new System.Drawing.Size(95, 28);
+            this.RadioButton2.TabIndex = 4;
+            this.RadioButton2.TabStop = true;
+            this.RadioButton2.Text = "sqrt(x)/x";
+            this.RadioButton2.UseVisualStyleBackColor = true;
+            this.RadioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // RadioButton3
+            // 
+            this.RadioButton3.AutoSize = true;
+            this.RadioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.RadioButton3.Location = new System.Drawing.Point(575, 252);
+            this.RadioButton3.Name = "RadioButton3";
+            this.RadioButton3.Size = new System.Drawing.Size(78, 28);
+            this.RadioButton3.TabIndex = 5;
+            this.RadioButton3.TabStop = true;
+            this.RadioButton3.Text = "x^2+3";
+            this.RadioButton3.UseVisualStyleBackColor = true;
+            this.RadioButton3.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // textField1
+            // 
+            this.textField1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textField1.Location = new System.Drawing.Point(168, 119);
+            this.textField1.Name = "textField1";
+            this.textField1.Size = new System.Drawing.Size(481, 29);
+            this.textField1.TabIndex = 6;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label.Location = new System.Drawing.Point(324, 394);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 24);
+            this.label.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textField1);
+            this.Controls.Add(this.RadioButton3);
+            this.Controls.Add(this.RadioButton2);
+            this.Controls.Add(this.RadioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CheckBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+           
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox CheckBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton RadioButton1;
+        private System.Windows.Forms.RadioButton RadioButton2;
+        private System.Windows.Forms.RadioButton RadioButton3;
+        private System.Windows.Forms.TextBox textField1;
+        private System.Windows.Forms.Label label;
+
+       
+        
+    }
+
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace lab2SH
+{
+    public partial class Form1 : Form
+    {
+        public static bool isNumeric(String str)
+        {
+            
+            return int.TryParse(str, out _);
+        }
+        public void Calculate(int choice)
+        {
+            double result = 1;
+            try
+            {
+
+                double x = Convert.ToDouble(textField1.Text);
+                if (!isNumeric(textField1.Text)) throw new Exception("Use numeric format");
+
+                switch (choice)
+                {
+                    case 1:
+                        result = x * Math.Sin(x);
+                        RadioButton3.Checked = false;
+                        RadioButton2.Checked = false;
+                        RadioButton1.Select();
+                        break;
+                    case 2:
+                        result = Math.Sqrt(x) / x;
+                        RadioButton1.Checked = false;
+                        RadioButton3.Checked = false;
+                        RadioButton2.Select();
+                        break;
+                    case 3:
+                        result = x * x + 2;
+                        RadioButton1.Checked = false;
+                        RadioButton2.Checked = false;
+                        RadioButton3.Select();
+                        break;
+                    default:
+                        if (x <= 0)
+                        {
+                            result = x * Math.Sin(x);
+                            RadioButton3.Checked = false;
+                            RadioButton2.Checked = false;
+                            RadioButton1.Select();
+                        }
+                        if ((x > 0 && x < 3))
+                        {
+                            result = Math.Sqrt(x) / x;
+                            RadioButton1.Checked = false;
+                            RadioButton3.Checked = false;
+                            RadioButton2.Select();
+                        }
+                        if (x >= 3)
+                        {
+                            result = x * x + 2;
+                            RadioButton1.Checked = false;
+                            RadioButton2.Checked = false;
+                            RadioButton3.Select();
+                        }
+                        break;
+                }
+                if (CheckBox.Checked) result *= 2;
+                label.Text = result.ToString();
+                
+
+            }
+            catch (Exception ex)
+            {
+               
+                label.Text = "Некорректные данные";
+                RadioButton1.Checked = false;
+                RadioButton2.Checked = false;
+                RadioButton3.Checked = false;
+                
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculate(0);
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            int choice = 0;
+            if (RadioButton1.Checked) choice = 1;
+            if (RadioButton2.Checked) choice = 2;
+            if (RadioButton3.Checked) choice = 3;
+            Calculate(choice);
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            int choice = 0;
+            Console.WriteLine(e.ToString());
+            if(sender.Equals(RadioButton1)) choice = 1;
+            if(sender.Equals(RadioButton2)) choice = 2;
+            if(sender.Equals(RadioButton3)) choice = 3;
+
+            Calculate(choice);
+        }
+    }
+
+}
